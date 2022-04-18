9dbae5c82ff1cdedfc335f3d8912fb4435084e1c
ElshanAli <elshanml@code.edu.az>
2022-04-18 17:24:56 +0400

    Added task added number

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int n = number;
+
+            Console.WriteLine(AddNumber(n)); 
+           
+        }
+        public static double AddNumber(int number)
+        {
+            int n = number;
+            int count = 0;
+            Console.WriteLine("Enter the number you want to add to the beginning:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            while (number != 0)
+            {
+                number /= 10;
+                count++;
+
+            }
+            double result = (m * (Math.Pow(10, count++)) + n);
+            return result;
+        }
+
+}
+}
